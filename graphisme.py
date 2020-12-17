aaec0a66f4a9f7bc67d3cf5cd87f41bf9bffb3d1
theo-pannethier <you@example.com>
2020-12-17 08:36:08 +0100

    ToDo graphisme

--- graphisme.py
+++ graphisme.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 """
+Programme gerant la partie graphique.
+
+ToDo:
+    -faire le canvas
+    -faire le bouton qui declanche la partie
+    -bouton pour quitter le jeu
+    -menu proposant differantes possibilitées
+    -inserer image alien et vaisseau
+Theo Pannethier / Jeffrey Simon
+17/12/2020
 """
 
 

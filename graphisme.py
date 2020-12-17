27de6036d3eb4ba522817b83079d03055b025eb7
theo-pannethier <you@example.com>
2020-12-17 12:06:49 +0100

    interface graphique

--- graphisme.py
+++ graphisme.py
@@ -3,9 +3,7 @@
 Programme gerant la partie graphique.
 
 ToDo:
-    -faire le canvas
-    -faire le bouton qui declanche la partie
-    -bouton pour quitter le jeu
+    -faire le bouton qui declanche la partie ~ 
     -menu proposant differantes possibilitées
     -inserer image alien et vaisseau
 Theo Pannethier / Jeffrey Simon
@@ -13,3 +11,29 @@ Theo Pannethier / Jeffrey Simon
 """
 
 
+from  tkinter import Tk,Button,Frame,PhotoImage,Canvas
+
+
+    
+Fenetre = Tk()
+Fenetre.geometry('1200x600+75+20')
+Fond = PhotoImage(file = 'FondJeu.gif')
+
+canva = Canvas(Fenetre,height=600 , width=1100)
+item = canva.create_image(0,0,anchor='nw',image = Fond )
+canva.grid(row=0, column=0)
+
+menu = Frame(Fenetre)
+menu.grid(row=0, column=1)
+QuitBouton = Button(menu,text= "Quit", fg = 'red',width=10,
+                    command = Fenetre.destroy)
+NewGameBouton = Button(menu,text= "New game", fg = 'red',width=10)
+NewGameBouton.pack(padx=0,pady=0)
+QuitBouton.pack(padx=0,pady=100)
+
+
+Fenetre.mainloop()
+
+
+
+#Fenetre.coords
